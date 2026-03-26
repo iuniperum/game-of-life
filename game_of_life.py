1d5ce547e9d076278db862b0f95900bd29149ef0
iuniperum <julpietrzak147@gmail.com>
2026-03-26 20:36:32 +0100

    update

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
 import random
+import time
 
 cell_width = 20
 cell_height = 20
@@ -27,22 +28,42 @@ def grid(matrix, s, x, y):
             else:
                 pygame.draw.rect(SCREEN, WHITE, rect)
 
-#def count_neighbourhoods(matrix[i][j])
 
-#def determine_state(matrix, i, j):
-#    if (matrix[i][j] )
+def count_alive(matrix, i, j):
+    alive = 0
+    for x in range (i - 1, i + 2):
+        for y in range (j - 1, j + 2):
+            try:
+                if x == i and y == j:
+                    continue
+                if matrix[x][y] == 1:
+                    alive += 1
+            except:
+                pass
+    return alive
+
+def determine_state(matrix, x, y):
+    for i in range(0, x):
+        for j in range(0, y):
+            curr_state = matrix[i][j]
+            alive_neigh = count_alive(matrix, i, j)
+            print(alive_neigh)
+            if curr_state == 1 and (alive_neigh < 2 or alive_neigh > 3):
+                matrix[i][j] = 0
+            if curr_state == 0 and alive_neigh == 3:
+                matrix[i][j] = 1
 
 pygame.init()
 
 #a = int(input("Number of cells horizontal: "))
 #b = int(input("Number of cells vertical: "))
-a = 300
-b = 300
+a = 30
+b = 30
 
 size = 1000/a
 
 #population = int(input("Number of alive cells at the start: "))
-population = 100
+population = 200
 
 board = np.zeros((int(a), int(b)))
 random_population(board)
@@ -57,7 +78,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    new_board = board.copy()
+    determine_state(new_board, a, b)
+    board = new_board.copy()
+    time.sleep(1)
     pygame.display.flip()
 
 pygame.quit()
